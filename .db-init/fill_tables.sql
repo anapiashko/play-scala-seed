e59f035ebe995aab079a8cc5aeb4082fb5c92d90
Anastasiya Piashko <anapiashko@gmail.com>
2023-02-27 19:28:18 +0400

    added filling db

---
+++ .db-init/fill_tables.sql
@@ -0,0 +1,6 @@
+INSERT INTO people (person_id, name, age) VALUES (1, 'Marta', 23);
+INSERT INTO people (person_id, name, age) VALUES (2, 'Mike', 17);
+INSERT INTO people (person_id, name, age) VALUES (3, 'Bob', 56);
+INSERT INTO people (person_id, name, age) VALUES (4, 'Sabina', 8);
+INSERT INTO people (person_id, name, age) VALUES (5, 'Tommy', 34);
+INSERT INTO people (person_id, name, age) VALUES (6, 'Greg', 40);
